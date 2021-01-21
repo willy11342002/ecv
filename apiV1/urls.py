bd0ce5bc4342625d7a0190a12a5ff94c746d1d51
劉定威 <tingwei.liu.19930301@gmail.com>
2021-01-21 15:13:01 +0800

    加入每日計算API

--- apiV1/urls.py
+++ apiV1/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     re_path(r'^lineitem/summary/(?P<usageaccountid>\d+)/?$', views.SummaryItem.as_view()),
+    re_path(r'^lineitem/summary/day/(?P<usageaccountid>\d+)/?$', views.SummaryItemByDays.as_view()),
 ]
--- apiV1/views.py
+++ apiV1/views.py
@@ -22,3 +22,25 @@ class SummaryItem(View):
 
         return JsonResponse(items)
 
+
+class SummaryItemByDays(View):
+    def get(self, request, usageaccountid):
+        items = models.LineItem.objects
+        items = items.select_related('product')
+        items = items.filter(UsageAccountId=usageaccountid)
+        items = items.order_by('product')
+
+        res = {}
+        for product, its in groupby(items, key=lambda x: x.product):
+            res[product.ProductName] = {}
+            its = list(its)
+            sday = min(its, key=lambda x: x.UsageStartDate).UsageStartDate.date()
+            eday = max(its, key=lambda x: x.UsageEndDate).UsageEndDate.date()
+
+            for i in range(0, (eday - sday).days+1):
+                mday = sday + datetime.timedelta(days=i)
+                today_its = filter(lambda x: x.UsageStartDate.date() <= mday <= x.UsageEndDate.date(), its)
+                today_its = map(lambda x: x.UsageAmount, today_its)
+                res[product.ProductName][mday.strftime(r'%Y/%m/%d')] = f'{sum(today_its):.2f}'
+
+        return JsonResponse(res)
