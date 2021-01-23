9c811632cb2eac7962b4414fa8762062f2b1c311
LAPTOP-312THIB9\tingw <tingwei.liu.19930301@gmail.com>
2021-01-23 22:37:35 +0800

    設定檔Debug Mode調整

--- ECV/settings.py
+++ ECV/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-s^#a#de=k@hy1@_r*7y9rve=gjy9diqw&k7w$-(_o65)sjcdk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
